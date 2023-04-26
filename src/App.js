0926b5148f979b4d1c1303dc9780003c538e40f6
Meirmanov1751 <kassimovd00@mail.com>
2023-04-26 19:43:55 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginForm from "./pages/auth/login/LoginForm";
+import {Route, Routes} from "react-router";
+import HeaderContainer from "./components/layout/header/header.container";
+import SingInForm from "./pages/auth/singIn/SingInForm";
+import ExecutorList from "./pages/executor/executorlist";
+import CustomerList from "./pages/customer/customerList";
+import OrderList from "./pages/order/orderList";
+import ProductList from "./pages/product/productList";
+import CreateOrderPage from "./pages/order/CreateOrder/CreateOrder";
+import CreateProductPage from "./pages/product/CreateProduct/CreateProduct";
+import {useContext, useEffect} from "react";
+import {Context} from "./index";
+import PostsList from "./pages/posts/postsList";
+import HomePage from "./pages/home/home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {Store1} = useContext(Context)
+
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            Store1.checkAuth()
+        }
+    }, [])
+
+    return (
+        <div className="App">
+            <HeaderContainer/>
+            <Routes>
+                <Route path={'/'} element={<HomePage/>}></Route>
+                <Route path={'/login'} element={<LoginForm/>}></Route>
+                <Route path={'/singin'} element={<SingInForm/>}></Route>
+                <Route path={'/executors'} element={<ExecutorList/>}></Route>
+                <Route path={'/customers'} element={<CustomerList/>}></Route>
+                <Route path={'/orders'} element={<OrderList/>}></Route>
+                <Route path={'/products'} element={<ProductList/>}></Route>
+                <Route path={'/orders_create'} element={<CreateOrderPage/>}></Route>
+                <Route path={'/products_create'} element={<CreateProductPage/>}></Route>
+                <Route path={'/posts'} element={<PostsList/>}></Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Store, {store} from "./store/store";
+import {BrowserRouter} from "react-router-dom";
+import * as PropTypes from "prop-types";
+import {Provider} from "react-redux";
+
+var Store1 = new Store()
+
+export const Context = createContext({
+    Store1,
+})
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Context.Provider value={{
+            Store1
+        }}>
+            <BrowserRouter>
+                <Provider store={store}>
+                    <App/>
+                </Provider>
+            </BrowserRouter>
+        </Context.Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
